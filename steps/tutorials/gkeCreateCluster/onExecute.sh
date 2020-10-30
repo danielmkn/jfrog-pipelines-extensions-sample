3a07ac1d995741631daca315fb7bc38cd96329fc
danielmkn <danielmi@jfrog.com>
2020-10-30 11:39:12 -0700

    k8s config test

--- steps/tutorials/gkeCreateCluster/onExecute.sh
+++ steps/tutorials/gkeCreateCluster/onExecute.sh
@@ -1,8 +1,8 @@
 run_kubectl_command() {
   echo "onExecute step"
     ls -al
-    echo "$int_gcpK8sPartnershipProject_jsonKey" | tee /tmp/serviceaccount_jfrog_gc_mp.json
-    gcloud auth activate-service-account --key-file=/tmp/serviceaccount_jfrog_gc_mp.json
+    #echo "$int_gcpK8sPartnershipProject_jsonKey" | tee /tmp/serviceaccount_jfrog_gc_mp.json
+    #gcloud auth activate-service-account --key-file=/tmp/serviceaccount_jfrog_gc_mp.json
     echo "Test if k8s configuration persist in the steps"
     kubectl get pods --all-namespaces
     kubectl get services --all-namespaces
