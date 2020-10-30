f4b5c97cc1e6763c03b6f27516d48dd639ee3372
danielmkn <danielmi@jfrog.com>
2020-10-29 17:15:02 -0700

    GKE

--- resources/tutorials/gkeClusterResource/onOutput.sh
+++ resources/tutorials/gkeClusterResource/onOutput.sh
@@ -1,6 +1,8 @@
 test_output() {
   echo "resource name: $1"
   printenv
+
+  gcloud -v
 }
 
 execute_command "test_output %%context.resourceName%%"
--- steps/tutorials/gkeCreateCluster/onExecute.sh
+++ steps/tutorials/gkeCreateCluster/onExecute.sh
@@ -1,17 +1,15 @@
 checkHealth() {
-  local success=true
-  local url=$(find_step_configuration_value "healthCheckUrl")
-  {
-    local statusCode=$(curl --silent --output /dev/stderr --write-out "%{http_code}" "$url")
-  } || exitCode=$?
-  if test $statusCode -ne 200; then
-    export success=false
-    echo "Health check failed with statusCode: $statusCode & exitCode: $exitCode for url: $url"
-  else
-    echo "Health check succeeded"
-  fi
-
-  $success
+  echo "onExecute test"
+    ls -al
+    ls -lad /tmp
+    sudo chmod 1777 /tmp
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    sudo apt-get install apt-transport-https ca-certificates gnupg
+    sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+    sudo apt-get update && sudo apt-get install google-cloud-sdk
+    gcloud -v
+    echo $int_gcpK8sMarketplaceProject_jsonKey | tee /tmp/serviceaccount_jfrog_gc_mp.json
+    gcloud auth activate-service-account --key-file=/tmp/serviceaccount_jfrog_gc_mp.json
 }
 
 execute_command checkHealth
