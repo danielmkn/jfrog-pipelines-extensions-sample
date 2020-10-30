3ac7d9ff2098f42ed1ac10e8461b68fd1d43f849
danielmkn <danielmi@jfrog.com>
2020-10-30 12:20:53 -0700

    k8s config test

--- resources/tutorials/gkeClusterResource/onInput.sh
+++ resources/tutorials/gkeClusterResource/onInput.sh
@@ -1,14 +1,9 @@
-test_input() {
+list_env_variables() {
   echo "Resource name: $1"
   echo "Environment variables:"
   printenv
   echo "Gcloud version:"
   gcloud -v
-  #echo "gcloud container clusters get-credentials $res_gkeClusterResource_gkeClusterName --zone $res_gkeClusterResource_gkeClusterZone --project $res_gkeClusterResource_googleCloudProj"
-  #echo "$int_gcpK8sPartnershipProject_jsonKey" | tee /tmp/serviceaccount_jfrog_gc_partner.json
-  #gcloud auth activate-service-account --key-file=/tmp/serviceaccount_jfrog_gc_partner.json
-  #gcloud container clusters get-credentials $res_gkeClusterResource_gkeClusterName --zone $res_gkeClusterResource_gkeClusterZone --project $res_gkeClusterResource_googleCloudProj
-  #kubectl get pods --all-namespaces
 }
 
-execute_command "test_input %%context.resourceName%%"
+execute_command "test_input %%context.list_env_variables%%"
--- resources/tutorials/gkeClusterResource/onOutput.sh
+++ resources/tutorials/gkeClusterResource/onOutput.sh
@@ -1,7 +1,8 @@
-test_output() {
+list_cluster_pods() {
   echo "Resource name: $1"
-  echo "Output"
+  echo "Cluster pods in all namespaces:"
+  kubectl get pods --all-namespaces
 
   }
 
-execute_command "test_output %%context.resourceName%%"
+execute_command "test_output %%context.list_cluster_pods%%"
