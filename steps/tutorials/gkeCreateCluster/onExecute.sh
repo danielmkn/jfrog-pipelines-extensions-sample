077c79a4331cbcbc0993aca775170753bc4917a4
danielmkn <danielmi@jfrog.com>
2020-10-30 13:20:28 -0700

    output resources test

--- steps/tutorials/gkeCreateCluster/onExecute.sh
+++ steps/tutorials/gkeCreateCluster/onExecute.sh
@@ -2,6 +2,9 @@ run_kubectl_command() {
   echo "onExecute step"
     ls -al
     printenv
+    echo $machine-type
+    echo "11111"
+    echo "%%context.machine-type%%"
     #gcloud container clusters get-credentials $res_gkeClusterResource_gkeClusterName --zone $res_gkeClusterResource_gkeClusterZone --project $res_gkeClusterResource_googleCloudProj
 #    gcloud container clusters create $newGkeClusterName --zone $newGkeClusterZone \
 #      --node-locations $newGkeClusterZone --num-nodes $num-nodes $enable-autoscaling \
