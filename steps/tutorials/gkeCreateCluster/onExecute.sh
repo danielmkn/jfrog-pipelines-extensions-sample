445f24762a4fb37ba0bd6faa67c287be38c831cc
danielmkn <danielmi@jfrog.com>
2020-10-30 12:59:07 -0700

    output resources test

--- steps/tutorials/gkeCreateCluster/onExecute.sh
+++ steps/tutorials/gkeCreateCluster/onExecute.sh
@@ -1,11 +1,16 @@
 run_kubectl_command() {
   echo "onExecute step"
     ls -al
-    gcloud container clusters get-credentials $res_gkeClusterResource_gkeClusterName --zone $res_gkeClusterResource_gkeClusterZone --project $res_gkeClusterResource_googleCloudProj
-    echo "Test if k8s configuration persist in the steps"
-    kubectl get pods --all-namespaces
-    echo "External IP addresses in the cluster:"
-    kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="ExternalIP")].address}'
+    printenv
+    #gcloud container clusters get-credentials $res_gkeClusterResource_gkeClusterName --zone $res_gkeClusterResource_gkeClusterZone --project $res_gkeClusterResource_googleCloudProj
+#    gcloud container clusters create $newGkeClusterName --zone $newGkeClusterZone \
+#      --node-locations $newGkeClusterZone --num-nodes $num-nodes $enable-autoscaling \
+#      --machine-type $machine-type \
+#      --min-nodes $min-nodes --max-nodes $max-nodes --project $res_gkeClusterResource_googleCloudProj
+
+
+
+
 }
 
 execute_command run_kubectl_command
