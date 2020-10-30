194fbe492f868b4ca9d5a5c4058e407b268e7bbf
danielmkn <danielmi@jfrog.com>
2020-10-30 11:49:36 -0700

    k8s config test

--- resources/tutorials/gkeClusterResource/onInput.sh
+++ resources/tutorials/gkeClusterResource/onInput.sh
@@ -7,8 +7,8 @@ test_input() {
   #echo "gcloud container clusters get-credentials $res_gkeClusterResource_gkeClusterName --zone $res_gkeClusterResource_gkeClusterZone --project $res_gkeClusterResource_googleCloudProj"
   #echo "$int_gcpK8sPartnershipProject_jsonKey" | tee /tmp/serviceaccount_jfrog_gc_partner.json
   #gcloud auth activate-service-account --key-file=/tmp/serviceaccount_jfrog_gc_partner.json
-  gcloud container clusters get-credentials $res_gkeClusterResource_gkeClusterName --zone $res_gkeClusterResource_gkeClusterZone --project $res_gkeClusterResource_googleCloudProj
-  kubectl get pods --all-namespaces
+  #gcloud container clusters get-credentials $res_gkeClusterResource_gkeClusterName --zone $res_gkeClusterResource_gkeClusterZone --project $res_gkeClusterResource_googleCloudProj
+  #kubectl get pods --all-namespaces
 }
 
 execute_command "test_input %%context.resourceName%%"
