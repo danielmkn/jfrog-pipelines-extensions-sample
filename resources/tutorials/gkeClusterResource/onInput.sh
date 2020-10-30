828ad19fca77547272d24ada654ec912d4e67aeb
danielmkn <danielmi@jfrog.com>
2020-10-29 17:53:07 -0700

    onInput

--- resources/tutorials/gkeClusterResource/onInput.sh
+++ resources/tutorials/gkeClusterResource/onInput.sh
@@ -1,11 +1,9 @@
 test_input() {
   echo "resource name: $1"
   printenv
-  echo "Installing gcloud"
-  python -V
+  echo "Testing"
   gcloud -v
-  sudo apt-get update && sudo apt-get --only-upgrade install kubectl google-cloud-sdk google-cloud-sdk-app-engine-grpc google-cloud-sdk-pubsub-emulator google-cloud-sdk-app-engine-go google-cloud-sdk-firestore-emulator google-cloud-sdk-cloud-build-local google-cloud-sdk-datastore-emulator google-cloud-sdk-app-engine-python google-cloud-sdk-cbt google-cloud-sdk-bigtable-emulator google-cloud-sdk-app-engine-python-extras google-cloud-sdk-datalab google-cloud-sdk-app-engine-java
-  gcloud -v
+  echo $int_gcpK8sPartnershipProject_jsonKey
 
 
 }
--- steps/tutorials/gkeCreateCluster/onExecute.sh
+++ steps/tutorials/gkeCreateCluster/onExecute.sh
@@ -3,13 +3,14 @@ checkHealth() {
     ls -al
     ls -lad /tmp
     sudo chmod 1777 /tmp
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    sudo apt-get install apt-transport-https ca-certificates gnupg
-    sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-    sudo apt-get update && sudo apt-get install google-cloud-sdk
-    gcloud -v
-    echo $int_gcpK8sMarketplaceProject_jsonKey | tee /tmp/serviceaccount_jfrog_gc_mp.json
-    gcloud auth activate-service-account --key-file=/tmp/serviceaccount_jfrog_gc_mp.json
+    printenv
+#    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+#    sudo apt-get install apt-transport-https ca-certificates gnupg
+#    sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+#    sudo apt-get update && sudo apt-get install google-cloud-sdk
+#    gcloud -v
+#    echo $int_gcpK8sMarketplaceProject_jsonKey | tee /tmp/serviceaccount_jfrog_gc_mp.json
+#    gcloud auth activate-service-account --key-file=/tmp/serviceaccount_jfrog_gc_mp.json
 }
 
 execute_command checkHealth
