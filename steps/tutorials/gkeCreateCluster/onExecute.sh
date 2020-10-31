60a7ac9f3d1640cde1895b657a0929ea9fef4c37
danielmkn <danielmi@jfrog.com>
2020-10-30 17:47:44 -0700

    removed echoing vars

--- steps/tutorials/gkeCreateCluster/onExecute.sh
+++ steps/tutorials/gkeCreateCluster/onExecute.sh
@@ -2,9 +2,9 @@ run_kubectl_command() {
   echo "onExecute step"
     ls -al
     printenv
-    echo ${machine-type}
-    echo "11111"
-    echo "%%context.machine-type%%"
+#    echo ${machine-type}
+#    echo "11111"
+#    echo "%%context.machine-type%%"
 
     #gcloud container clusters get-credentials $res_gkeClusterResource_gkeClusterName --zone $res_gkeClusterResource_gkeClusterZone --project $res_gkeClusterResource_googleCloudProj
 
