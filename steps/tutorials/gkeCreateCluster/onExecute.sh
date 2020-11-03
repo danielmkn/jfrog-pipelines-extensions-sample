f69bd09aba6ccb84ff8bb3ba16c4c5882bd57639
danielmkn <danielmi@jfrog.com>
2020-11-02 16:09:13 -0800

    Delete commented out code

--- steps/tutorials/gkeCreateCluster/onExecute.sh
+++ steps/tutorials/gkeCreateCluster/onExecute.sh
@@ -2,19 +2,6 @@ run_kubectl_command() {
   echo "onExecute step"
     ls -al
     printenv
-#    echo ${machine-type}
-#    echo "11111"
-#    echo "%%context.machine-type%%"
-
-    #gcloud container clusters get-credentials $res_gkeClusterResource_gkeClusterName --zone $res_gkeClusterResource_gkeClusterZone --project $res_gkeClusterResource_googleCloudProj
-
-#    gcloud container clusters create $newGkeClusterName --zone $gkeClusterZone \
-#      --node-locations $newGkeClusterZone --num-nodes $num-nodes $clusterOptions \
-#      --machine-type $machine-type \
-#      --min-nodes $min-nodes --max-nodes $max-nodes --project $res_gkeClusterResource_googleCloudProj
-
-
-
 
 }
 
