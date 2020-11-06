6157ac64a36f9bc9527f999204b727213796bb2e
danielmkn <danielmi@jfrog.com>
2020-11-06 14:37:53 -0800

    AKS cluster resource and steps

--- resources/azure/aksClusterResource/onOutput.sh
+++ resources/azure/aksClusterResource/onOutput.sh
@@ -7,7 +7,7 @@ save_cluster_parameters() {
   echo "Cluster name: " $step_configuration_aksClusterName
   echo "Cluster location: " $step_configuration_location
   echo "VM size: " $step_configuration_vmSize
-  echo "Node count: " $step_configuration_numNodes
+  echo "Node count: " $step_configuration_nodeCount
   echo "Minimum number of nodes: " $step_configuration_minCount
   echo "Maximum number of nodes: " $step_configuration_maxCount
   echo "Cluster options: " $step_configuration_clusterOptions
@@ -16,7 +16,7 @@ save_cluster_parameters() {
                                   aksClusterName=$step_configuration_aksClusterName \
                                   location=$step_configuration_location \
                                   vmSize=$step_configuration_vmSize \
-                                  numNodes=$step_configuration_numNodes \
+                                  numNodes=$step_configuration_nodeCount \
                                   minCount=$step_configuration_minCount \
                                   maxCount=$step_configuration_maxCount \
                                   clusterOptions=$step_configuration_clusterOptions
