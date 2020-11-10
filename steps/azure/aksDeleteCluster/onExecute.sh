7d9ccb0c42c2c02e44a0fc86a895cf118815e41e
danielmkn <danielmi@jfrog.com>
2020-11-09 20:47:48 -0800

    delete cluster step

--- steps/azure/aksDeleteCluster/onExecute.sh
+++ steps/azure/aksDeleteCluster/onExecute.sh
@@ -6,14 +6,19 @@ delete_aks_cluster() {
   echo "### TESTING VALUES ###"
   echo "Resource group name: " $res_aksClusterResource_azureResourceGroup
   echo "Cluster name: " $res_aksClusterResource_aksClusterName
-  echo "Cluster location: " $res_aksClusterResource_location
+  echo "Cluster location: " $res_azureResGroupResource_location
   echo "VM size: " $res_aksClusterResource_vmSize
   echo "Node count: " $res_aksClusterResource_numNodes
   echo "Minimum number of nodes: " $res_aksClusterResource_minCount
   echo "Maximum number of nodes: " $res_aksClusterResource_maxCount
   echo "Cluster options: " $res_aksClusterResource_clusterOptions
 
-  az aks show --resource-group ${res_aksClusterResource_azureResourceGroup} --name ${res_aksClusterResource_aksClusterName}
+  az aks show --resource-group ${res_azureResGroupResource_azureResourceGroup} --name ${res_aksClusterResource_aksClusterName}
+  az aks delete --name ${res_aksClusterResource_aksClusterName} \
+                --resource-group ${res_azureResGroupResource_azureResourceGroup} \
+                --yes
+
+
 
 }
 
