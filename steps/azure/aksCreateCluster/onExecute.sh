3c4e988f2f6c42c1a3165deaf4804fb894d98a8f
danielmkn <danielmi@jfrog.com>
2020-11-09 20:13:57 -0800

    Create RG resource and step

--- steps/azure/aksCreateCluster/onExecute.sh
+++ steps/azure/aksCreateCluster/onExecute.sh
@@ -3,14 +3,11 @@ create_aks_cluster() {
     printenv
 
     # Create resource group
-    az group create -l $step_configuration_location -n $step_configuration_azureResourceGroup
-    echo "##### Create AKS cluster ${step_configuration_azureResourceGroup} #####"
-    az aks create --resource-group ${step_configuration_azureResourceGroup} --name ${step_configuration_aksClusterName} \
+    echo "##### Create AKS cluster in ${res_azureResGroupResource_azureResourceGroup} #####"
+    az aks create --resource-group ${res_azureResGroupResource_azureResourceGroup} --name ${step_configuration_aksClusterName} \
                   --node-count ${step_configuration_nodeCount} --node-vm-size ${step_configuration_vmSize} \
                   --service-principal $int_azurePartnership_appId --client-secret $int_azurePartnership_password \
                   ${step_configuration_clusterOptions}
-
-
 
 }
 
