72ab8c11626df15c1fe63302e5d41f2ff84d8b3c
danielmkn <danielmi@jfrog.com>
2020-11-12 11:16:45 -0800

    Chart version added

--- steps/helm/helmInstall/onExecute.sh
+++ steps/helm/helmInstall/onExecute.sh
@@ -3,8 +3,15 @@ helm_install() {
     printenv
 
     echo "##### Install helm chart ${step_configuration_repoName} #####"
+    if [ -z "$step_configuration_chartVersion" ]
+    then
+      VERSION=""
+    else
+      VERSION="--version=${step_configuration_chartVersion}"
+    fi
 
     helm install $step_configuration_helmName $step_configuration_helmChart \
+                 $VERSION \
                  $int_helmInstallParameters_setFlags
 
 }
