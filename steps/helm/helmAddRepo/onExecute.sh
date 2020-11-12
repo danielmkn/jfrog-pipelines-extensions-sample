a2e24bb42648a9e69ff96ac2eecd9795a2dc10f3
danielmkn <danielmi@jfrog.com>
2020-11-12 12:33:03 -0800

    testing helm add repo

--- steps/helm/helmAddRepo/onExecute.sh
+++ steps/helm/helmAddRepo/onExecute.sh
@@ -2,10 +2,19 @@ add_helm_repository() {
   echo "onExecute step"
     printenv
 
+    echo "### test connect to gke cluster ###"
+
+    gcloud container clusters get-credentials created-by-pipelines-extension --zone us-central1-c \
+     --project jfrog-partnership-team
+
     echo "##### Add helm repository ${step_configuration_repoName} #####"
 
+    helm version
+    echo "helm init"
     helm init
+    echo "helm repo add"
     helm repo add $step_configuration_repoName $step_configuration_repoUrl
+    echo "helm update"
     helm repo update
 
 }
