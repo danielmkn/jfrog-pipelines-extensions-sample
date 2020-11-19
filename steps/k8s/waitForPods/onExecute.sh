a1ce02c2d3e15dc71dc228f8c9b756dce47573be
danielmkn <danielmi@jfrog.com>
2020-11-18 18:02:50 -0800

    wait for pods

--- steps/k8s/waitForPods/onExecute.sh
+++ steps/k8s/waitForPods/onExecute.sh
@@ -11,30 +11,37 @@ wait_for_pods() {
     --project ${res_gkeClusterResource_googleCloudProj}
   fi
 
+  local NAMESPACE="${res_helmInstallResource_namespace:-default}"
 
   echo "#####################"
   echo "### Wait for pods ###"
   echo "#####################"
-  DEPLOYMENT_NOT_FINISHED=$(kubectl get deployment | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "READY" | grep -v "1/1"| wc -l)
-  STATEFULSET_NOT_FINISHED=$(kubectl get statefulset | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "READY" | grep -v "1/1"| wc -l)
-  JOBS_NOT_FINISHED=$(kubectl get jobs | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "COMPLETIONS" | grep -v "1/1"| wc -l)
-  PODS_ERROR=$(kubectl get pods | grep -i 'err' | wc -l)
-  while [ "${DEPLOYMENT_NOT_FINISHED}" -gt 0 ] || [ "${STATEFULSET_NOT_FINISHED}" -gt 0 ] || [ "${JOBS_NOT_FINISHED}" -gt 0 ]
-  do
-    sleep 15
-    DEPLOYMENT_NOT_FINISHED=$(kubectl get deployment | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "READY" | grep -v "1/1"| wc -l)
-    STATEFULSET_NOT_FINISHED=$(kubectl get statefulset | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "READY" | grep -v "1/1"| wc -l)
-    JOBS_NOT_FINISHED=$(kubectl get jobs | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "COMPLETIONS" | grep -v "1/1"| wc -l)
-    PODS_ERROR=$(kubectl get pods | grep -i 'err' | wc -l)
-    if [ "${PODS_ERROR}" -gt 0 ]
-    then
-      echo "Deployment went to error state. Failed!"
-      exit 1
-    else
-      kubectl get pods
-    fi
-  done
+  DEPLOYMENT_NOT_FINISHED=$(kubectl get deployment -n "$NAMESPACE" | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "READY" | grep -v "1/1"| wc -l)
+  STATEFULSET_NOT_FINISHED=$(kubectl get statefulset -n "$NAMESPACE" | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "READY" | grep -v "1/1"| wc -l)
+  JOBS_NOT_FINISHED=$(kubectl get jobs -n "$NAMESPACE" | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "COMPLETIONS" | grep -v "1/1"| wc -l)
+  PODS_ERROR=$(kubectl get pods -n "$NAMESPACE" | grep -i 'err' | wc -l)
 
+  if ! kubectl get pods -n "$NAMESPACE" -o json | jq -re '.items[] // empty' 1&> /dev/null
+  then
+      echo "No resources in the cluster/namespace. Failed!"
+      exit 1
+  else
+    while [ "${DEPLOYMENT_NOT_FINISHED}" -gt 0 ] || [ "${STATEFULSET_NOT_FINISHED}" -gt 0 ] || [ "${JOBS_NOT_FINISHED}" -gt 0 ]
+    do
+      sleep 15
+      DEPLOYMENT_NOT_FINISHED=$(kubectl get deployment -n "$NAMESPACE" | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "READY" | grep -v "1/1"| wc -l)
+      STATEFULSET_NOT_FINISHED=$(kubectl get statefulset -n "$NAMESPACE" | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "READY" | grep -v "1/1"| wc -l)
+      JOBS_NOT_FINISHED=$(kubectl get jobs -n "$NAMESPACE" | awk '{$2=$2};1' | cut -f2 -d ' ' | grep -v "COMPLETIONS" | grep -v "1/1"| wc -l)
+      PODS_ERROR=$(kubectl get pods -n "$NAMESPACE" | grep -i 'err' | wc -l)
+      if [ "${PODS_ERROR}" -gt 0 ]
+      then
+        echo "Deployment went to error state. Failed!"
+        exit 1
+      else
+        kubectl get pods $NAMESPACE
+      fi
+    done
+  fi
 }
 
 execute_command wait_for_pods
