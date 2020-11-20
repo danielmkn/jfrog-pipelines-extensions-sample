f4f5218b82a5610408cebdd028e2e24bf51b6a8d
danielmkn <danielmi@jfrog.com>
2020-11-20 12:33:42 -0800

    Slack integration test

--- steps/azure/aksCreateCluster/onFailure.sh
+++ steps/azure/aksCreateCluster/onFailure.sh
@@ -1,5 +1,20 @@
 failure() {
-    echo "FAILED"
+
+  local slackIntegrationName=$(get_integration_name --type "Slack")
+  if [ ! -z "$slackIntegrationName" ]; then
+    local notifyOnSuccess=$(find_step_configuration_value "notifyOnSuccess")
+    if [ -z "$notifyOnSuccess" ]; then
+      notifyOnSuccess=false
+    fi
+    if [ "$notifyOnSuccess" == "true" ]; then
+      echo "Sending success notification"
+      send_notification "$slackIntegrationName" --text "Failed to create AKS cluster ${step_configuration_aksClusterName}"
+    else
+      echo "notifyOnSuccess is set to false, skipping notification"
+    fi
+  else
+    echo "Slack integration is not added, skipping notification"
+  fi
 }
 
 execute_command failure
--- steps/azure/azureCreateResourceGroup/onSuccess.sh
+++ steps/azure/azureCreateResourceGroup/onSuccess.sh
@@ -1,6 +1,21 @@
 success() {
-    echo "SUCCESS"
-    echo "Azure resource group $step_configuration_azureResourceGroup has been successfully created."
+
+  local slackIntegrationName=$(get_integration_name --type "Slack")
+  if [ ! -z "$slackIntegrationName" ]; then
+    local notifyOnSuccess=$(find_step_configuration_value "notifyOnSuccess")
+    local azureResourceGroup=$(find_step_configuration_value "azureResourceGroup")
+    if [ -z "$notifyOnSuccess" ]; then
+      notifyOnSuccess=false
+    fi
+    if [ "$notifyOnSuccess" == "true" ]; then
+      echo "Sending success notification"
+      send_notification "$slackIntegrationName" --text "Azure resource group ${azureResourceGroup} has been successfully created"
+    else
+      echo "notifyOnSuccess is set to false, skipping notification"
+    fi
+  else
+    echo "Slack integration is not added, skipping notification"
+  fi
 
 }
 
